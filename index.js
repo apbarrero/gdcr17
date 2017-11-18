effdd9a6dbfa96766d5053f2a2b219821edfc586
Deibit <doliva@refineriaweb.com>
2017-11-18 11:52:34 +0100

    Constructor test green

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 class Grid {
-   constructor(width, height) {
-   }
+    constructor(width, height) {
+        this.grid = new Array(height);
+        for (let i = 0; i < height; i++)
+            this.grid[i] = new Array(width);
+    }
+
+    getHeight() {
+        return this.grid.length
+    }
+
+    getWidth() {
+        return this.grid[0].length
+    }
+
 }
 
 module.exports = {Grid};
--- spec/kata.spec.js
+++ spec/kata.spec.js
@@ -5,7 +5,7 @@ describe('Grid', ()=>{
    const Grid = gol.Grid;
    it('should create grid', ()=>{
       let g = new Grid(2, 3);
-      expect(g.width).toBe(2);
-      expect(g.height).toBe(3);
+      expect(g.getWidth()).toBe(2);
+      expect(g.getHeight()).toBe(3);
    });
 });
