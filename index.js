069934e1c402f893063affb286672095cff2a545
Antonio Pérez Barrero <apbarrero@gmail.com>
2017-11-18 12:04:46 +0100

    Pass create error test

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 class Grid {
     constructor(width, height) {
+       if (width <= 0 || height <= 0)
+          throw Error('Wrong grid dimension values');
+       this.createGrid(width, height);
+    }
+
+   createGrid(width, height) {
         this.grid = new Array(height);
         for (let i = 0; i < height; i++)
             this.grid[i] = new Array(width);
-    }
+   }
 
     getHeight() {
         return this.grid.length
