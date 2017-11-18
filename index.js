688978dbd873b9c2023cd890d7fb99eda494305f
Deibit <doliva@refineriaweb.com>
2017-11-18 10:38:31 +0100

    Jasmine init

---
+++ index.js
@@ -0,0 +1,14 @@
+function Kata() {
+};
+
+Kata.prototype.hello = "Hello";
+
+Kata.prototype.functionName = function(param) {
+    // return true/false
+}
+
+Kata.prototype.play = function(number) {
+    // call function
+    return true;
+};
+
---
+++ spec/kata.spec.js
@@ -0,0 +1,8 @@
+
+require('jasmine');
+
+describe("Kata", function() {
+    it('should value be equal to result', function() {
+        expect((1).toString()).toEqual('1');
+    });
+});
