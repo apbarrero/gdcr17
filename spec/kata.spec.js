33a629b868054a319b532fb01cb3431b209a1b3f
Deibit <doliva@refineriaweb.com>
2017-11-18 11:22:48 +0100

    Red test

--- spec/kata.spec.js
+++ spec/kata.spec.js
@@ -8,4 +8,14 @@ describe("Grid", function() {
       expect(g.width).toBe(2);
       expect(g.height).toBe(3);
    });
+
+
+   it('show print grid', function() {
+       const width = 2;
+       const height = 3;
+       let g = new Grid(width, height);
+       const length = (width + 1) * height
+      expect(g.toString().length).toBe(length);
+   });
+
 });
