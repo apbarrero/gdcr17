b42c768956b8677632e0f67d73c5d83c379dd15f
Deibit <doliva@refineriaweb.com>
2017-11-18 11:23:12 +0100

    Removed commented code

--- index.js
+++ index.js
@@ -10,18 +10,3 @@ class Grid {
 }
 
 module.exports = {Grid};
-
-//function Kata() {
-//};
-
-//Kata.prototype.hello = "Hello";
-
-//Kata.prototype.functionName = function(param) {
-    //// return true/false
-//}
-
-//Kata.prototype.play = function(number) {
-    //// call function
-    //return true;
-//};
-
